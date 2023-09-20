13e2405539fd26210e9475dc49f395e416a3fac1
Maksim <max-goryachev@ya.ru>
2023-09-20 15:15:09 +0600

    add arrow

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const project = document.querySelector('a[href*="#"]');
 const projectID = project.getAttribute('href').substring(1);
+
+
 project.addEventListener('click', e => {
     e.preventDefault();
     document.getElementById(projectID).scrollIntoView({
         behavior: 'smooth',
         block: 'start'
     })
+})
+
+
+const upBtn = document.querySelector('.up-btn')
+upBtn.addEventListener('click', scrollToTop)
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+window.addEventListener('scroll', () => {
+    let scroll = document.querySelector('.up-btn').classList.toggle('active', window.scrollY > 200)
+
 })
